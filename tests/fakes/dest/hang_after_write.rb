99f8c24b018afebebfcafb20eef07fd45706bc86
Alex Young <alex@bytemark.co.uk>
2012-07-02 13:00:30 +0100

    Tweak a timeout to prevent an intermittent test failure

--- tests/fakes/dest/hang_after_write.rb
+++ tests/fakes/dest/hang_after_write.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-# Open a socket, say hello, receive a write, then sleep for > 6
-# seconds. This should tell the source that the write has gone MIA,
-# and we expect a reconnect.
+# Open a socket, say hello, receive a write, then sleep for >
+# MS_REQUEST_LIMIT_SECS seconds. This should tell the source that the
+# write has gone MIA, and we expect a reconnect.
 
 require 'flexnbd/fake_dest'
 include FlexNBD::FakeDest
@@ -15,7 +15,7 @@ read_request( client_sock1 )
 
 t = Thread.start do
   client_sock2 = accept( sock, "Timed out waiting for a reconnection",
-                        FlexNBD::MS_REQUEST_LIMIT_SECS + 1 )
+                        FlexNBD::MS_REQUEST_LIMIT_SECS + 2 )
   client_sock2.close
 end
 
